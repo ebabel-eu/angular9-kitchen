9d387ec08f5fecbb75e288663b5559361fa0b539
Thomas Amar <hello@ebabel.eu>
2020-04-26 19:24:35 +0200

    Implement hamburger.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Angular9 Kitchen';
-  lead: string = 'Organise your shopping list and recipes';
+  collapsed: boolean = true;
 
   constructor() { }
 
