11bca2f752df1382711f4cdf788ec6961f95d896
Thomas Amar <hello@ebabel.eu>
2020-04-27 17:44:54 +0200

    Progress the course exercises.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.interface';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    {
+      name: 'kesar mangoes',
+      amount: 4,
+    },
+    {
+      name: 'teaspoon cardamom powder',
+      amount: 0.5,
+    },
+    {
+      name: 'pinch saffron strands',
+      amount: 1,
+    },
+    {
+      name: 'teaspoon dry ginger powder',
+      amount: 0.25,
+    },
+    {
+      name: 'jug of water or milk',
+      amount: 1,
+    },
+  ];
 
   constructor() { }
 
